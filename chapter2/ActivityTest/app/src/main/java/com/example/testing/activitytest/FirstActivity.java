00f04cb4ef2bfae7cd03e3525653318e1706aa98
陈卢鑫 <953546398@qq.com>
2018-09-11 22:17:13 +0800

    init commit

---
+++ chapter2/ActivityTest/app/src/main/java/com/example/testing/activitytest/FirstActivity.java
@@ -0,0 +1,144 @@
+package com.example.testing.activitytest;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    private static final String TAG = "FirstActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        Log.i(TAG, "onCreate: Hi");
+
+        Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "This is a short toast",
+                    Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "This is a long toast",
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+        Button button_finish = findViewById(R.id.button_finish);
+        button_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 销毁活动
+                finish();
+
+            }
+        });
+
+        // 跳转
+        Button button_jump = findViewById(R.id.button_jump);
+        button_jump.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = "Hi SecondActivity";
+//                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+
+                Intent intent = new Intent("com.example.testing.activitytest.ACTION_START");
+                intent.addCategory("com.example.testing.activitytest.MY_CATEGORY");
+
+                intent.putExtra("extra_data", data);
+                intent.putExtra("extra_int", 100);
+                startActivity(intent);
+
+            }
+        });
+
+        // 另一个跳转
+        Button button_jump2 = findViewById(R.id.button_jump2);
+        button_jump2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
+
+            }
+        });
+
+
+        // 浏览网页
+        Button button_browser = findViewById(R.id.button_browser);
+        button_browser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                // 注意url要符合格式，必须带有http
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+
+        // 打电话
+        Button button_tel = findViewById(R.id.button_tel);
+        button_tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        // 让菜单显示
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "Add sth", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "Remove sth", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                String returnData = data.getStringExtra("data_return");
+                    Log.d(TAG, returnData);
+                }
+                break;
+            default:
+        }
+    }
+}
---
+++ chapter2/ActivityTest/app/src/main/java/com/example/testing/activitytest/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.example.testing.activitytest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private static final String TAG = "SecondActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+
+//        Intent intent = getIntent();
+//        String data = intent.getStringExtra("extra_data");
+//        int num = intent.getIntExtra("extra_int", 0);
+//
+//        Log.d(TAG, data);
+//        Log.d(TAG, "" + num);
+
+        Button button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello First Activity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Hello First Activity I'm back");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
